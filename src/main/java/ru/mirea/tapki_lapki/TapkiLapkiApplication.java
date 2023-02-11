b7bbe6101852f8ef9e5c8f564e3707d01e374847
Mikhail OKhapkin <mikhail.okhapkin@yandex.ru>
2023-02-11 11:51:37 +0300

    Initialization of the project

---
+++ src/main/java/ru/mirea/tapki_lapki/TapkiLapkiApplication.java
@@ -0,0 +1,13 @@
+package ru.mirea.tapki_lapki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapkiLapkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TapkiLapkiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mirea/tapki_lapki/TapkiLapkiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.tapki_lapki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapkiLapkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
