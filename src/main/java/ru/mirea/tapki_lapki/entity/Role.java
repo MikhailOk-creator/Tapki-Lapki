1ffd451edb61ea76f7c5b5c1bcf602f50efade21
Mikhail Okhapkin <mikhail.okhapkin@yandex.ru>
2023-03-27 21:09:10 +0300

    Add new Role

--- src/main/java/ru/mirea/tapki_lapki/entity/Role.java
+++ src/main/java/ru/mirea/tapki_lapki/entity/Role.java
@@ -3,7 +3,7 @@ package ru.mirea.tapki_lapki.entity;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER, ADMIN, SUPER_ADMIN;
+    USER, MANAGER, ADMIN, SUPER_ADMIN;
     @Override
     public String getAuthority() {
         return null;
