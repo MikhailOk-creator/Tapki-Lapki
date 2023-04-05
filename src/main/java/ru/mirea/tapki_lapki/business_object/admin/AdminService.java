88ac26bd43f50a6e782ba7291ef8054c1fea2072
Mikhail Okhapkin <72760972+MikhailOk-creator@users.noreply.github.com>
2023-04-05 18:50:57 +0300

    Added methods for admin

--- src/main/java/ru/mirea/tapki_lapki/business_object/admin/AdminService.java
+++ src/main/java/ru/mirea/tapki_lapki/business_object/admin/AdminService.java
@@ -1,18 +1,69 @@
 package ru.mirea.tapki_lapki.business_object.admin;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import ru.mirea.tapki_lapki.business_object.order.Order;
+import ru.mirea.tapki_lapki.business_object.order.Status;
+import ru.mirea.tapki_lapki.business_object.user.Role;
 import ru.mirea.tapki_lapki.business_object.user.User;
 import ru.mirea.tapki_lapki.business_object.user.UserRepo;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AdminService {
     private final UserRepo userRepo;
 
     public List<User> allUsers() {
         return userRepo.findAll();
+    }
+
+    public void addUserEmployee(String username, String password, String email, Role role) {
+        try {
+            User user = new User();
+
+            log.info("Data in .env: " + "\n" +
+                    "Username: " + username + '\n' +
+                    "Email: " + email + '\n');
+
+            if ((username != null && !username.equals(""))  &&
+                    (password != null && !password.equals("")) &&
+                    (email != null && !email.equals(""))) {
+                user.setUsername(username);
+                user.setPassword(password);
+                user.setEmail(email);
+
+                user.setActive(true);
+                user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+                user.setRoles(Set.of(role));
+                userRepo.save(user);
+                log.info("User with username {} saved");
+            } else {
+                log.error("Main data is not full!");
+            }
+        } catch (Exception e) {
+            log.error("Something went wrong in the process of writing to the database!");
+        }
+    }
+
+    public void changeStatusOfOrder(Long orderId, Status status) {
+        // Order order =
+    }
+
+    public void changeUserActivity(Long id) {
+        try {
+            User user = userRepo.findByUsername(userRepo.getReferenceById(id).getUsername());
+            user.setActive(!user.isActive());
+            userRepo.save(user);
+            log.info("User's (id: {}, username: {}) activity status has been changed ({})",
+                    user.getId(), user.getUsername(), user.isActive());
+        } catch (Exception e) {
+            log.error("Wrong input data");
+        }
     }
 }
