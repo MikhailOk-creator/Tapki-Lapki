1c8b167a828cd518bfdbf4b164c8178e10d342b6
Mikhail Okhapkin <mikhail.okhapkin@yandex.ru>
2023-04-13 17:36:52 +0300

    Rename client's role

--- src/main/java/ru/mirea/tapki_lapki/business_object/user/Role.java
+++ src/main/java/ru/mirea/tapki_lapki/business_object/user/Role.java
@@ -3,7 +3,7 @@ package ru.mirea.tapki_lapki.business_object.user;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER, MANAGER, ADMIN, SUPER_ADMIN;
+    CLIENT, MANAGER, ADMIN, SUPER_ADMIN;
     @Override
     public String getAuthority() {
         return null;
