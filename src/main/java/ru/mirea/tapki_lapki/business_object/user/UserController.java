26929e88631d248841884df6b37fe9e2f545f457
Mikhail Okhapkin <72760972+MikhailOk-creator@users.noreply.github.com>
2023-04-14 17:12:49 +0300

    Fix registration

--- src/main/java/ru/mirea/tapki_lapki/business_object/user/UserController.java
+++ src/main/java/ru/mirea/tapki_lapki/business_object/user/UserController.java
@@ -1,6 +1,8 @@
 package ru.mirea.tapki_lapki.business_object.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import ru.mirea.tapki_lapki.business_object.admin.AdminService;
@@ -15,8 +17,11 @@ public class UserController {
         return userService.getUserById(id);
     }
 
-    @QueryMapping
-    public void createUser (String username, String password, String email, String role) {
-        userService.registerUser(username, password, email, role);
+    @MutationMapping
+    public User createUser (@Argument String username,
+                            @Argument String password,
+                            @Argument String email,
+                            @Argument String role) {
+        return userService.registerUser(username, password, email, role);
     }
 }
